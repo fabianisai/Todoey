7908a58c420c6158634b1bf9aa87cf7bc07e7d24
Fabian Isai <fabian.isai@gmail.com>
2018-12-05 09:15:07 -0800

    convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by FabianIsai on 12/4/18.
+//  Copyright © 2018 FabianIsai. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
